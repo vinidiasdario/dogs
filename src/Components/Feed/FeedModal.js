229dfb3a8da741e490bf872e562da610ca59b9b0
Vinicius Dario <vncs_dias@hotmail.com>
2022-10-22 12:36:37 -0300

    second comit
    Co-authored-by: Guilherme Amado <GuilhermeAmado@users.noreply.github.com>

---
+++ src/Components/Feed/FeedModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { PHOTO_GET } from "../../Api";
+import UseFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "../Photo/PhotoContent";
+import styles from "./FeedModal.module.css";
+
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = UseFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className={styles.modal}>
+      <img src={photo.src} alt="" />
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
+
+export default FeedModal;
