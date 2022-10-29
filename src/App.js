4a56c796023b6d7339ef9e2a0cde49c4be989402
Vinicius Dario <vncs_dias@hotmail.com>
2022-10-29 12:07:38 -0300

    29.10.2022 - User Profile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./Components/Login/Login";
 import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="login/*" element={<Login />} />
             <Route path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
